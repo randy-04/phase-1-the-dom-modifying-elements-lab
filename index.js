444519df0408988b19961bfe80b26093e5eeb7df
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2022-11-29 22:00:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+
+const getMain = document.getElementById("main");
+getMain.remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "Randy is the champion";
